7b5a78461009e32ed4227acd57910b3f8e145965
allenjanian <allen@janianventures.com>
2013-08-19 18:15:51 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// form toggle buttons
+
+$(document).ready(function(){
+ 
+  $("#grapeButton").click(function(){
+    $("#grapeList").toggle(1000);
+  });
+
+  $("#wineButton").click(function(){
+    $("#wineList").toggle(1000);
+  });
+});
+
+// remove other button on either click
+
